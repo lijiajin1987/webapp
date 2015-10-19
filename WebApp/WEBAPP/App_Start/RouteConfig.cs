287ebccbc890dd8e5dbb81716253e0223861e44d
lijiajin1987 <lijiajin1987@gmail.com>
2015-10-19 18:40:33 +0800

    webapp
    Signed-off-by: lijiajin1987 <lijiajin1987@gmail.com>

---
+++ WebApp/WEBAPP/App_Start/RouteConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WEBAPP
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            #region 静态页面路由映射
+            routes.MapPageRoute("","Home","~/Views/Home/index.html");
+            routes.MapPageRoute("", "test", "~/Views/test.html");
+            routes.MapPageRoute("", "test2", "~/Views/test2.html");
+            routes.MapPageRoute("", "why", "~/Views/why/index.html");
+            routes.MapPageRoute("", "QuestionList", "~/Views/why/QuestionList.html");//QuestionList
+            #endregion
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
